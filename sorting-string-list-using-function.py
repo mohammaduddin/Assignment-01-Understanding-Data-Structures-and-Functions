172b92c0c85c284af3af0a350d14ef4e37c944cc
Mohammad Uddin <muddin2429@gmail.com>
2025-05-12 14:53:01 -0400

    Fruit list in alphabeic order

---
+++ sorting-string-list-using-function.py
@@ -0,0 +1,16 @@
+# Problem: 02
+# Write a function that sorts a list of strings alphabetically.
+# ["apple", "banana", "cherry", "kiwi", "grape"]
+
+# before sorting
+fruit_list = ["blue berry", "lychee","cherry", "kiwi", "coconut", "apple", "banana", "grape"]
+print("Fruit list before sorting : ", fruit_list)
+
+# after sorting
+fruit_list.sort()
+print("Fruit list in alphabeic order: ", fruit_list)
+
+
+# OUTPUT:
+# Fruit list before sorting :  ['blue berry', 'lychee', 'cherry', 'kiwi', 'coconut', 'apple', 'banana', 'grape']
+# Fruit list in alphabeic order:  ['apple', 'banana', 'blue berry', 'cherry', 'coconut', 'grape', 'kiwi', 'lychee']
